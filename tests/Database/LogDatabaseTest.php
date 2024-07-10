07cbb70499742bc3ff8184516d214ac4f5872807
Laurent Muller <laurent.muller@haslerrail.com>
2024-07-10 10:13:09 +0200

    Create LogDatabaseTest.php

---
+++ tests/Database/LogDatabaseTest.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the Calculation package.
+ *
+ * (c) bibi.nu <bibi@bibi.nu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Database;
+
+use App\Database\AbstractDatabase;
+use App\Database\LogDatabase;
+use App\Entity\Log;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(AbstractDatabase::class)]
+#[CoversClass(LogDatabase::class)]
+class LogDatabaseTest extends TestCase
+{
+    public function testInsertLog(): void
+    {
+        $database = new LogDatabase(AbstractDatabase::IN_MEMORY);
+
+        $actual = $database->getRecordsCount('sy_Log');
+        self::assertSame(0, $actual);
+
+        $log = Log::instance(1)
+            ->setMessage('Message');
+        $actual = $database->insertLog($log);
+        self::assertTrue($actual);
+
+        $actual = $database->getRecordsCount('sy_Log');
+        self::assertSame(1, $actual);
+
+        $database->close();
+    }
+}

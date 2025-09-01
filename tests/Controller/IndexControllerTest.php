8b02f5b9b6e27f2a30a73832319df470b15e8241
Laurent Muller <bibi@bibi.nu>
2025-09-01 06:25:25 +0200

    Update IndexControllerTest.php

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -27,12 +27,25 @@ class IndexControllerTest extends ControllerTestCase
         yield ['/', self::ROLE_SUPER_ADMIN];
         yield ['/?custom=1&restrict=1', self::ROLE_USER];
         yield ['/?custom=1&restrict=1&count=8', self::ROLE_USER];
+
         yield ['/hide/catalog', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_POST, true];
         yield ['/hide/month', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_POST, true];
         yield ['/hide/state', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_POST, true];
+
+        yield ['/content', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_GET, true];
+        yield ['/content?custom=1&restrict=1', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_GET, true];
+        yield ['/content?custom=1&restrict=1&count=8', self::ROLE_USER, Response::HTTP_OK,  Request::METHOD_GET, true];
     }
 
-    public function testInvalidRequest(): void
+    public function testInvalidContent(): void
+    {
+        $this->loginUsername(self::ROLE_USER);
+        $this->client->request(Request::METHOD_GET, '/content');
+        $actual = $this->client->getResponse()->getStatusCode();
+        self::assertSame(Response::HTTP_BAD_REQUEST, $actual);
+    }
+
+    public function testInvalidHide(): void
     {
         $this->loginUsername(self::ROLE_USER);
         $this->client->request(Request::METHOD_POST, '/hide/catalog');

c9578c9a3147108da9a942ad35b7d2c71957247e
Bibi <laurent.muller@haslerrail.com>
2025-10-27 16:23:33 +0100

    Update PdfChartColorsTest.php

--- tests/Pdf/Colors/PdfChartColorsTest.php
+++ tests/Pdf/Colors/PdfChartColorsTest.php
@@ -20,41 +20,35 @@ use PHPUnit\Framework\TestCase;
 
 class PdfChartColorsTest extends TestCase
 {
-    private PdfChartColors $colors;
-
-    #[\Override]
-    protected function setUp(): void
-    {
-        $this->colors = new PdfChartColors();
-    }
-
     /**
-     * @return \Generator<int, array{int, int, int}>
+     * @return \Generator<int, array{PdfChartColors, int, int, int}>
      */
     public static function getColors(): \Generator
     {
-        yield [54, 162, 235]; // blue
-        yield [255, 99, 132]; // red
-        yield [255, 159, 64]; // orange
-        yield [255, 205, 86]; // yellow
-        yield [75, 192, 192]; // green
-        yield [153, 102, 255]; // purple
-        yield [201, 203, 207]; // grey
+        $colors = new PdfChartColors();
+        yield [$colors, 54, 162, 235]; // blue
+        yield [$colors, 255, 99, 132]; // red
+        yield [$colors, 255, 159, 64]; // orange
+        yield [$colors, 255, 205, 86]; // yellow
+        yield [$colors, 75, 192, 192]; // green
+        yield [$colors, 153, 102, 255]; // purple
+        yield [$colors, 201, 203, 207]; // grey
         // new cycle
-        yield [54, 162, 235]; // blue
+        yield [$colors, 54, 162, 235]; // blue
     }
 
     public function testCount(): void
     {
         $expected = 7;
-        $actual = $this->colors->count();
+        $colors = new PdfChartColors();
+        $actual = $colors->count();
         self::assertSame($expected, $actual);
     }
 
     #[DataProvider('getColors')]
-    public function testNext(int $red, int $green, int $blue): void
+    public function testNext(PdfChartColors $colors, int $red, int $green, int $blue): void
     {
-        $actual = $this->colors->next();
+        $actual = $colors->next();
         self::assertSameColor($actual, $red, $green, $blue);
     }
 
